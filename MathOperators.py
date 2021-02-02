efdc789c7015bf210822bbaf9fe8d6b5ea76ab6f
Natan Siller Laurett <natan_siller@hotmail.com>
2021-02-01 21:09:01 -0300

    matrix multiplication supporting 1 dimension

--- MathOperators.py
+++ MathOperators.py
@@ -1,21 +1,35 @@
 
 
 def matMult(A, B):
-    if len(A[0]) != len(B):
+    if not isinstance(A[0],list):
+        aRows = 1
+        aCols = len(A)
+    else:
+        aRows = len(A)
+        aCols = len(A[0])
+    
+    if not isinstance(B[0],list):
+        bRows = 1
+        bCols = len(B)
+    else:
+        bRows = len(B)
+        bCols = len(B[0])
+
+    if aCols != bRows:
         print("Dimensions must agree. Returning...")
         return []
 
-    aRows = len(A)
-    bCols = len(B[0])
-    aCols = len(A[0])
     out = [[0 for i in range(bCols)] for j in range(aRows)]
 
     for i in range(aRows):
         for j in range(bCols):
             for k in range(aCols):
                 out[i][j] += A[i][k] * B[k][j]
+
+    if len(out[0]) == 1 and len(out) == 1:
+        out = out[0]
     return out
 
-A  = [[1, 2, 3], [4, 5, 6]]
-B = [[1, 4], [2, 5], [3, 6]]
+A  = [[1, 2, 3]]
+B = [[1], [2], [3]]
 print(matMult(A,B))
